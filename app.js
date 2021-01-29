6c898c47c0947c5f7016c9bdda1e9e000327c498
Avaneesh Kumar <avaneeshk098@gmail.com>
2021-01-29 08:33:18 +0530

    Fixed scrolling issues

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.getElementsByClassName("chat-profiles-mobile")[0].addEventListener('cli
     document.getElementsByClassName("chat-profiles-mobile")[0].style.display = "none";
     document.getElementsByClassName('chat-menu-mobile')[0].style.display = "none";
     document.getElementsByClassName('chat-box-container-mobile')[0].style.display = "block";
-    document.getElementsByClassName('chat-messages-container-mobile')[0].scrollTop = document.getElementsByClassName('chat-messages-container-mobile')[0].scrollHeight
+    document.getElementsByClassName('chat-messages-container-mobile')[0].scrollTop = document.getElementsByClassName('chat-messages-container-mobile')[0].scrollHeight 
 })
 
 document.getElementsByClassName("message-box-desktop")[0].scrollTop = document.getElementsByClassName("message-box-desktop")[0].scrollHeight
